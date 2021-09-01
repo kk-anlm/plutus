ae68157a58ba326f8868472ed23050e9d6d03e9d
Jamie Bertram <jamie.bertram@tweag.io>
2021-09-01 16:08:04 -0400

    Create input base component type

--- marlowe-dashboard-client/src/Input/Base.purs
+++ marlowe-dashboard-client/src/Input/Base.purs
@@ -1,52 +1,104 @@
 module Input.Base
-  ( Input
-  , Output(..)
+  ( Action
+  , HTML
+  , Message(..)
+  , Props
+  , Query(..)
+  , Slot
   , State
-  , Action(..)
-  , defaultInput
-  , handleAction
-  , initialState
-  , inputProps
-  , renderError
+  , defaultProps
+  , mkInputProps
+  , component
   ) where
 
 import Prelude
 import Control.MonadPlus (guard)
-import Css as Css
-import DOM.HTML.Indexed (HTMLinput)
-import DOM.HTML.Indexed.InputType (InputType)
-import Data.Array (catMaybes)
-import Data.Lens (Lens', assign)
+import Data.Lens (Lens', assign, use)
 import Data.Lens.Record (prop)
-import Data.Maybe (Maybe(..), fromMaybe, isNothing)
+import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
+import Effect.Aff.Class (class MonadAff)
 import Halogen as H
-import Halogen.Css (classNames)
+import Halogen.Extra (blur, focus)
 import Halogen.HTML as HH
 import Halogen.HTML.Events.Extra (onValueInput_)
 import Halogen.HTML.Properties as HP
+import Web.Event.Event (Event)
+import Web.HTML (HTMLElement)
 
 -------------------------------------------------------------------------------
--- Input
+-- Data
 -------------------------------------------------------------------------------
--- | Input fields shared by all input component types.
-type Input r
-  = { additionalCss :: Array String
-    , id_ :: Maybe String
-    , readOnly :: Boolean
+type Props msg slots m
+  = { render :: State -> HTML msg slots m
     , value :: String
     , error :: Maybe String
-    | r
     }
 
-_additionalCss :: forall a r. Lens' { additionalCss :: a | r } a
-_additionalCss = prop (SProxy :: SProxy "additionalCss")
+data Message msg
+  = ValueChanged String
+  | Emitted msg
 
-_id_ :: forall a r. Lens' { id_ :: a | r } a
-_id_ = prop (SProxy :: SProxy "id_")
+data Query a
+  = Focus a
+  | Blur a
+  | SetText String a
+  | GetState (State -> a)
+  | GetInputElement (HTMLElement -> a)
 
-_readOnly :: forall a r. Lens' { readOnly :: a | r } a
-_readOnly = prop (SProxy :: SProxy "readOnly")
+data Action msg slots m
+  = PropsReceived (Props msg slots m)
+  | Changed String
+  | Raise msg
+
+type InnerState msg slots m
+  = { props :: Props msg slots m
+    , pristine :: Boolean
+    }
+
+defaultProps :: forall msg slots m. Props msg slots m
+defaultProps =
+  { render: const $ HH.text ""
+  , value: ""
+  , error: Nothing
+  }
+
+initialState :: forall msg slots m. Props msg slots m -> InnerState msg slots m
+initialState props =
+  { props
+  , pristine: true
+  }
+
+type State
+  = { value :: String
+    , error :: Maybe String
+    }
+
+innerStateToState :: forall msg slots m. InnerState msg slots m -> State
+innerStateToState { props: { value, error }, pristine } =
+  { value
+  , error: guard (not pristine) *> error
+  }
+
+type HTML msg slots m
+  = H.ComponentHTML (Action msg slots m) slots m
+
+type InputM msg slots m
+  = H.HalogenM
+      (InnerState msg slots m)
+      (Action msg slots m)
+      slots
+      (Message msg)
+      m
+
+type Slot msg id
+  = H.Slot Query (Message msg) id
+
+-------------------------------------------------------------------------------
+-- Optics
+-------------------------------------------------------------------------------
+_render :: forall a r. Lens' { render :: a | r } a
+_render = prop (SProxy :: SProxy "render")
 
 _value :: forall a r. Lens' { value :: a | r } a
 _value = prop (SProxy :: SProxy "value")
@@ -54,100 +106,94 @@ _value = prop (SProxy :: SProxy "value")
 _error :: forall a r. Lens' { error :: a | r } a
 _error = prop (SProxy :: SProxy "error")
 
--- | A default input value
-defaultInput :: Input ()
-defaultInput =
-  { additionalCss: []
-  , id_: Nothing
-  , readOnly: false
-  , value: ""
-  , error: Nothing
-  }
-
--------------------------------------------------------------------------------
--- Output
--------------------------------------------------------------------------------
--- | Output events that can be observed on an input
-data Output
-  = ValueChanged String
-  | Focused
-  | Blurred
-
--------------------------------------------------------------------------------
--- State
--------------------------------------------------------------------------------
--- | The internal state of a text input
-type State ri r
-  = { input :: Input ri
-    , pristine :: Boolean
-    | r
-    }
-
-_input :: forall a r. Lens' { input :: a | r } a
-_input = prop (SProxy :: SProxy "input")
+_props :: forall a r. Lens' { props :: a | r } a
+_props = prop (SProxy :: SProxy "props")
 
 _pristine :: forall a r. Lens' { pristine :: a | r } a
 _pristine = prop (SProxy :: SProxy "pristine")
 
--- | Initialize the state given input
-initialState :: forall r. Input r -> State r ()
-initialState input = { input, pristine: true }
-
--------------------------------------------------------------------------------
--- Action
--------------------------------------------------------------------------------
-data Action r
-  = ValueChangedInternal String
-  | FocusedInternal
-  | BlurredInternal
-  | InputReceived (Input r)
-
--------------------------------------------------------------------------------
--- handleAction
--------------------------------------------------------------------------------
-handleAction ::
-  forall m slots ri r.
-  Monad m =>
-  Action ri ->
-  H.HalogenM (State ri r) (Action ri) slots Output m Unit
-handleAction (ValueChangedInternal value) = do
-  assign (_input <<< _value) value
-  assign _pristine false
-  H.raise $ ValueChanged value
-
-handleAction FocusedInternal = do
-  H.raise Focused
-
-handleAction BlurredInternal = do
-  H.raise Blurred
-
-handleAction (InputReceived input) = do
-  assign _input input
+_stateValue :: forall msg slots m. Lens' (InnerState msg slots m) String
+_stateValue = _props <<< _value
 
 -------------------------------------------------------------------------------
 -- Render helpers
 -------------------------------------------------------------------------------
-inputProps ::
-  forall ri r.
-  InputType ->
-  (Boolean -> Array String) ->
-  State ri r ->
-  Array (HP.IProp HTMLinput (Action ri))
-inputProps inputType inputStyle state@{ input } =
-  catMaybes
-    [ Just $ HP.type_ inputType
-    , Just $ classNames $ inputStyle (isNothing $ visibleError state) <> input.additionalCss
-    , HP.id_ <$> input.id_
-    , Just $ HP.value input.value
-    , Just $ HP.readOnly input.readOnly
-    , Just $ onValueInput_ ValueChangedInternal
-    ]
+type InputProps r
+  = ( value :: String
+    , onInput :: Event
+    | r
+    )
 
-renderError :: forall w ri r. State ri r -> HH.HTML w (Action ri)
-renderError state =
-  HH.div
-    [ classNames Css.inputError ]
-    [ HH.text $ fromMaybe "" $ visibleError state ]
+inputRef :: H.RefLabel
+inputRef = H.RefLabel "input"
 
-visibleError :: forall ri r. State ri r -> Maybe String
-visibleError { input: { error }, pristine } = guard pristine *> error
+mkInputProps ::
+  forall msg slots m r.
+  String ->
+  Array (HH.IProp (InputProps r) (Action msg slots m)) ->
+  Array (HH.IProp (InputProps r) (Action msg slots m))
+mkInputProps value overrides =
+  [ HP.ref inputRef
+  , HP.value value
+  , onValueInput_ Changed
+  ]
+    <> overrides
+
+-------------------------------------------------------------------------------
+-- Component
+-------------------------------------------------------------------------------
+component ::
+  forall msg slots m.
+  MonadAff m =>
+  H.Component HH.HTML Query (Props msg slots m) (Message msg) m
+component =
+  H.mkComponent
+    { initialState
+    , render: \state -> state.props.render $ innerStateToState state
+    , eval:
+        H.mkEval
+          H.defaultEval
+            { handleAction = handleAction
+            , handleQuery = handleQuery
+            , receive = Just <<< PropsReceived
+            }
+    }
+
+handleAction ::
+  forall msg slots m.
+  MonadAff m =>
+  Action msg slots m ->
+  InputM msg slots m Unit
+handleAction = case _ of
+  PropsReceived props -> H.modify_ _ { props = props }
+  Changed value -> do
+    assign _stateValue value
+    assign _pristine false
+    H.raise $ ValueChanged value
+  Raise msg -> H.raise $ Emitted msg
+
+handleQuery ::
+  forall msg slots m a.
+  MonadAff m =>
+  Query a ->
+  InputM msg slots m (Maybe a)
+handleQuery = case _ of
+  Focus n -> do
+    focus inputRef
+    pure $ Just n
+  Blur n -> do
+    blur inputRef
+    pure $ Just n
+  SetText value n -> do
+    currentValue <- use _stateValue
+    when (currentValue /= value) do
+      assign _stateValue value
+      H.raise $ ValueChanged value
+    pure $ Just n
+  GetState q -> do
+    (Just <<< q <<< innerStateToState) <$> H.get
+  GetInputElement q -> do
+    map q <$> H.getHTMLElementRef inputRef
+
+raise :: forall msg slots m. msg -> Action msg slots m
+raise = Raise
--- web-common/src/Halogen/HTML/Extra.purs
+++ web-common/src/Halogen/HTML/Extra.purs
@@ -1,8 +1,36 @@
-module Halogen.HTML.Extra (mapComponent) where
+module Halogen.HTML.Extra (mapComponent, maybeHTML, maybeHTML', whenHTML) where
 
+import Prelude
 import Data.Bifunctor (bimap)
-import Data.Functor (map)
-import Halogen.HTML (ComponentHTML)
+import Data.Maybe (Maybe, maybe)
+import Halogen.HTML as HH
 
-mapComponent :: forall m a b slots. (a -> b) -> ComponentHTML a slots m -> ComponentHTML b slots m
+mapComponent ::
+  forall m a b slots.
+  (a -> b) ->
+  HH.ComponentHTML a slots m ->
+  HH.ComponentHTML b slots m
 mapComponent f = bimap (map f) f
+
+maybeHTML ::
+  forall action slots m a.
+  (a -> HH.ComponentHTML action slots m) ->
+  Maybe a ->
+  HH.ComponentHTML action slots m
+maybeHTML = maybe (HH.text "")
+
+maybeHTML' ::
+  forall action slots m a.
+  Maybe a ->
+  (a -> HH.ComponentHTML action slots m) ->
+  HH.ComponentHTML action slots m
+maybeHTML' = flip maybeHTML
+
+whenHTML ::
+  forall action slots m.
+  Boolean ->
+  HH.ComponentHTML action slots m ->
+  HH.ComponentHTML action slots m
+whenHTML cond html = case cond of
+  true -> html
+  false -> HH.text ""
